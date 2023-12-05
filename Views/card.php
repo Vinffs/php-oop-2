ae429ceb1adf09247c8a9e9793aa7165c21c2855
Vinffs <vin.demiranda@gmail.com>
2023-12-05 16:35:02 +0100

    added styling and logo

--- Views/card.php
+++ Views/card.php
@@ -1,55 +1,56 @@
 <!-- dynamic calls are from the construct of Movie.php -->
 <div class="col-12 col-md-4 col-lg-3 my-3">
   <div class="card h-100 border-0 ">
-    <img class="thumb" src="<?= $image ?>" class="card-img-top my-ratio" alt="<?= $title ?>">
-    <div class="card-body">
-      <h5 class="card-title text-center py-1">
+    <img class="<?php echo (isset($content) ? 'thumb' : 'gamePic') ?>" src="<?= $image ?>" class="card-img-top my-ratio"
+      alt="<?= $title ?>">
+    <div class="card-body pt-0">
+      <h5 class="card-title text-center py-2">
         <?= $title ?>
       </h5>
       <?php if (isset($content)) { ?>
-      <p class="card-text py-2">
-        <?= $content ?>
-      </p>
+        <p class="card-text py-2">
+          <?= $content ?>
+        </p>
       <?php } ?>
       <?php if (isset($custom)) { ?>
-      <div class="d-flex justify-content-between align-items-start">
-        <div>
-          <?= $custom ?>
+        <div class="d-flex justify-content-between align-items-start">
+          <div>
+            <?= $custom ?>
+          </div>
+          <div>
+            <img class="flag" src="./img/<?= $language ?>.svg" alt="<?= $language ?>">
+          </div>
         </div>
-        <div>
-          <img class="flag" src="./img/<?= $language ?>.svg" alt="<?= $language ?>">
-        </div>
-      </div>
       <?php } ?>
 
       <?php if (isset($genres)) { ?>
-      <div class="py-2">
-        <p class="mb-1 fw-bold">Genres:</p>
-        <?php foreach ($genres as $genre) { ?>
-        <div>
-          <?= $genre->name ?>
+        <div class="py-2">
+          <p class="mb-1 fw-bold">Genres:</p>
+          <?php foreach ($genres as $genre) { ?>
+            <div>
+              <?= $genre->name ?>
+            </div>
+          <?php } ?>
         </div>
-        <?php } ?>
-      </div>
       <?php } ?>
 
       <?php if (isset($authors)) { ?>
-      <div class="py-2">
-        <p class="mb-1 fw-bold">Authors:</p>
-        <?php foreach ($authors as $author) { ?>
-        <div>
-          <?= $author ?>
-        </div>
+        <div class="py-2">
+          <p class="mb-1 fw-bold">Authors:</p>
+          <?php foreach ($authors as $author) { ?>
+            <div>
+              <?= $author ?>
+            </div>
 
-        <?php } ?>
-      </div>
+          <?php } ?>
+        </div>
       <?php } ?>
 
       <?php if (isset($playtime)) { ?>
-      <div>
-        <span class="mb-1 fw-bold">Play Hours:</span>
-        <?= $playtime ?> Hours
-      </div>
+        <div>
+          <span class="mb-1 fw-bold">Play Hours:</span>
+          <?= $playtime ?> Hours
+        </div>
       <?php } ?>
 
       <div>
@@ -61,11 +62,11 @@
         <?= $price ?>$
       </div>
       <?php if ($sconto > 0) { ?>
-      <div>
-        <span class="mb-1 fw-bold">Discounted Price:</span>
-        <?= $price * (100 - $sconto) / 100 ?>$ (
-        <?= $sconto ?> % )
-      </div>
+        <div>
+          <span class="mb-1 fw-bold">Discounted Price:</span>
+          <?= $price * (100 - $sconto) / 100 ?>$ (
+          <?= $sconto ?> % )
+        </div>
       <?php } ?>
     </div>
 
--- Views/header.php
+++ Views/header.php
@@ -18,10 +18,31 @@
   <title>OOP Shop</title>
 </head>
 
-<body>
-  <header>
-    <div class="container text-center py-4">
-      <h1>OOP Shop</h1>
+<body class="bg-color">
+  <header class="container py-4">
+    <div class="text-center py-4">
+
     </div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary w-50 bg-color">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img class="logo" src="./img/logo.png" alt="logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active text-white px-3 fs-4" aria-current="page" href="index.php">Home</a>
+            <a class="nav-link text-white px-3 fs-4" href="index.php">Movies</a>
+            <a class="nav-link text-white px-3 fs-4" href="book.php">Books</a>
+            <a class="nav-link text-white px-3 fs-4" href="game.php">Games</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+
   </header>
   <main>
